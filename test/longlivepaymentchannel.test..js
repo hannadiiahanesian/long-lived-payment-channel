b8c7e2e4d6b174e647262fff19a5cd1b8f066436
hannadiiahanesian <59712525+hannadiiahanesian@users.noreply.github.com>
2020-07-22 13:20:08 -0400

    solved

---
+++ test/longlivepaymentchannel.test..js
@@ -0,0 +1,118 @@
+const LongLivedPaymentChannel = artifacts.require("LongLivedPaymentChannel");
+
+contract(
+  "Recipient should be able to withdraw amount and then close",
+  (accounts) => {
+    // declare all global variables here
+    let contractInstance;
+    let contractAddress;
+    let longLivedPaymentChannelTx;
+    const skey =
+      "dec072ad7e4cf54d8bce9ce5b0d7e95ce8473a35f6ce65ab414faea436a2ee86"; // private key
+    web3.eth.accounts.wallet.add(`0x${skey}`);
+    const masterAccount = accounts[0];
+    const sender = web3.eth.accounts.wallet[0].address;
+    const senderSkey = web3.eth.accounts.wallet[0].privateKey;
+    const recipient = accounts[1];
+    const closeDuration = 200;
+    const depositAmount = web3.utils.toWei("2", "ether");
+
+    // sender can open the channel (deploy contract and deposit funds)
+    before(async () => {
+      await web3.eth.sendTransaction({
+        from: masterAccount,
+        to: sender,
+        value: web3.utils.toWei("5", "ether"),
+        gas: 21000,
+      });
+
+      contractInstance = new web3.eth.Contract(LongLivedPaymentChannel.abi);
+
+      const gas = await contractInstance
+        .deploy({
+          data: LongLivedPaymentChannel.bytecode,
+          from: sender,
+          value: depositAmount,
+          arguments: [recipient, closeDuration],
+        })
+        .estimateGas();
+
+      longLivedPaymentChannelTx = await contractInstance
+        .deploy({
+          data: LongLivedPaymentChannel.bytecode,
+          arguments: [recipient, closeDuration],
+        })
+        .send({
+          from: sender,
+          gas,
+          value: depositAmount,
+        });
+
+      contractAddress = longLivedPaymentChannelTx.options.address;
+
+      const actualSender = await longLivedPaymentChannelTx.methods.sender().call({
+        from: recipient,
+      });
+      const actualRecipient = await longLivedPaymentChannelTx.methods.recipient().call({
+        from: accounts[2]
+      });
+      const actualCloseDuration = await longLivedPaymentChannelTx.methods.closeDuration().call({
+        from: accounts[2]
+      })
+      const actualDepositedAmount = await web3.eth.getBalance(contractAddress);
+
+      // assertions
+      assert.equal(actualSender, sender, "Sender is not as expected");
+      assert.equal(
+        actualDepositedAmount,
+        depositAmount,
+        "The deposited amount is as expected"
+      );
+      assert.equal(
+        actualRecipient,
+        recipient,
+        "The recipient is as expected"
+      );
+      assert.equal(actualCloseDuration, closeDuration, "closeDuration is not as expected")
+
+    });
+
+    it("recepient is able to withdraw", async () => {
+      const withdrawalSum = web3.utils.toWei("1", "ether");
+      const contractStartBalance = await web3.eth.getBalance(contractAddress);
+      const recipientStartBalance = await web3.eth.getBalance(recipient);
+
+      const msgHash = web3.utils.soliditySha3(
+        { t: "address", v: contractAddress },
+        { t: "uint256", v: withdrawalSum }
+      );
+
+      const signed = await web3.eth.accounts.sign(msgHash, senderSkey);
+
+      const withdrawalTX = await longLivedPaymentChannelTx.methods
+        .withdraw(withdrawalSum, signed.signature)
+        .send({ from: recipient });
+
+      const recipientAfterBalance = await web3.eth.getBalance(recipient);
+      const contractAfterBalance = await web3.eth.getBalance(contractAddress);
+
+      const tx = await web3.eth.getTransaction(withdrawalTX.transactionHash);
+
+      const Fee = web3.utils
+        .toBN(tx.gasPrice)
+        .mul(web3.utils.toBN(withdrawalTX.gasUsed));
+
+      const recipientSumBalance = web3.utils
+        .toBN(recipientStartBalance)
+        .add(web3.utils.toBN(withdrawalSum))
+        .sub(web3.utils.toBN(Fee));
+
+      const contractSumBalance = web3.utils
+        .toBN(contractStartBalance)
+        .sub(web3.utils.toBN(withdrawalSum));
+
+      assert.equal(recipientSumBalance, recipientAfterBalance, "Invalid recipient balance");
+      assert.equal(contractSumBalance, contractAfterBalance, "Invalid contract balance");
+    });
+  }
+);
